601569dedabf5ec6dde0109bfe834cff160902c1
Pierre Wieser <pierre@pwi-consulting.fr>
2024-02-01 13:21:04 +0100

    Have ttp.pl vars -logsdir -logsroot

--- Toops/ttp/vars.do.pl
+++ Toops/ttp/vars.do.pl
@@ -1,41 +1,33 @@
-# @(#) list various services objects
+# @(#) display internal TTP variables
 #
 # @(-) --[no]help              print this message, and exit [${opt_help_def}]
 # @(-) --[no]verbose           run verbosely [$opt_verbose_def]
-# @(-) --[no]services          list defined services [$opt_services_def]
-# @(-) --[no]workloads         list used workloads [$opt_workloads_def]
+# @(-) --[no]logsdir           display the current Toops logs directory path [$opt_logsdir_def]
+# @(-) --[no]logsroot          display the site-defined logs root path [$opt_logsroot_def]
 #
 # Copyright (@) 2023-2024 PWI Consulting
-#
 
 use Data::Dumper;
 
-use Mods::Dbms;
 use Mods::Services;
 
 my $TTPVars = Mods::Toops::TTPVars();
 
-my $opt_services_def = 'no';
-my $opt_services = false;
-my $opt_workloads_def = 'no';
-my $opt_workloads = false;
+my $opt_logsdir_def = 'no';
+my $opt_logsdir = false;
+my $opt_logsroot_def = 'no';
+my $opt_logsroot = false;
 
 # -------------------------------------------------------------------------------------------------
-# list the defined DBMS instances (which may be not all the running instances)
-sub listDbms(){
-	Mods::Services::listDefinedDBMSInstances();
+# list logsdir value - e.g. 'C:\INLINGUA\Logs\Toops\240201'
+sub listLogsdir(){
+	print " $TTPVars->{dyn}{logs_dir}".EOL;
 }
 
 # -------------------------------------------------------------------------------------------------
-# list the defined services (same than ttp.pl list -services)
-sub listServices(){
-	Mods::Services::listDefinedServices();
-}
-
-# -------------------------------------------------------------------------------------------------
-# list the defined workloads
-sub listWorkloads(){
-	Mods::Services::listUsedWorkloads();
+# list logsroot value - e.g. 'C:\INLINGUA\Logs\Toops'
+sub listLogsroot(){
+	print " $TTPVars->{config}{site}{toops}{logsRoot}".EOL;
 }
 
 # =================================================================================================
@@ -45,8 +37,8 @@ sub listWorkloads(){
 if( !GetOptions(
 	"help!"				=> \$TTPVars->{run}{help},
 	"verbose!"			=> \$TTPVars->{run}{verbose},
-	"services!"			=> \$opt_services,
-	"workloads!"		=> \$opt_workloads	)){
+	"logsdir!"			=> \$opt_logsdir,
+	"logsroot!"			=> \$opt_logsroot )){
 
 		Mods::Toops::msgOut( "try '$TTPVars->{command_basename} $TTPVars->{verb} --help' to get full usage syntax" );
 		Mods::Toops::ttpExit( 1 );
@@ -58,13 +50,12 @@ if( Mods::Toops::wantsHelp()){
 }
 
 Mods::Toops::msgVerbose( "found verbose='true'" );
-Mods::Toops::msgVerbose( "found services='$opt_services'" );
-Mods::Toops::msgVerbose( "found workloads='$opt_workloads'" );
+Mods::Toops::msgVerbose( "found logsdir='$opt_logsdir'" );
+Mods::Toops::msgVerbose( "found logsroot='$opt_logsroot'" );
 
 if( !Mods::Toops::errs()){
-	listDbms() if $opt_dbms;
-	listServices() if $opt_services;
-	listWorkloads() if $opt_workloads;
+	listLogsdir() if $opt_logsdir;
+	listLogsroot() if $opt_logsroot;
 }
 
 Mods::Toops::ttpExit();
