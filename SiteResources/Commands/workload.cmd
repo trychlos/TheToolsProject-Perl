790708ffe197c353afe5cd8966759671826387dc
Pierre Wieser <pierre@pwi-consulting.fr>
2024-02-24 02:41:53 +0100

    workload.cmd: improve logging

--- SiteResources/Commands/workload.cmd
+++ SiteResources/Commands/workload.cmd
@@ -4,10 +4,14 @@
 	rem Note 1: this workload.cmd adds itself the -nocolor option to every run command. Tou should take care that your run commands accept (if not honor) this command-line option.
 	set ME=[%~nx0 %1]
 	call :setLogFile %1
+	call :doExecute >>%LOGFILE% 2>&1
+	exit /b
+
+:doExecute
 	call :logLine executing %~f0 %*
 	set i=0
 	for /f "tokens=*" %%C in ('services.pl list -workload %1 -commands -hidden %2 %3 %4 %5 %6 %7 %8 %9 -nocolored ^| findstr /V "[services.pl list]"') do call :doCommand %%C %2 %3 %4 %5 %6 %7 %8 %9
-	services.pl workload_summary -workload %1 -commands res_command -start res_start -end res_end -rc res_rc -count %i% %2 %3 %4 %5 %6 %7 %8 %9 -nocolored >> %LOGFILE%
+	services.pl workload_summary -workload %1 -commands res_command -start res_start -end res_end -rc res_rc -count %i% %2 %3 %4 %5 %6 %7 %8 %9 -nocolored
     exit /b
 
 :doCommand
@@ -17,14 +21,14 @@
 	set res_command[%i%]=%*
 	set res_start[%i%]=%DATE:~6,4%-%DATE:~3,2%-%DATE:~0,2% %TIME%
 	call :logLine %ME% %*
-	%* >> %LOGFILE% 2>&1
+	%*
 	call :logLine %ME% RC=%ERRORLEVEL%
 	set res_rc[%i%]=%ERRORLEVEL%
 	set res_end[%i%]=%DATE:~6,4%-%DATE:~3,2%-%DATE:~0,2% %TIME%
 	exit /b
 
 :logLine
-	echo %DATE:~6,4%-%DATE:~3,2%-%DATE:~0,2% %TIME:~0,8% %* >> %LOGFILE%
+	echo %DATE:~6,4%-%DATE:~3,2%-%DATE:~0,2% %TIME:~0,8% %*
 	exit /b
 
 :setLogFile
