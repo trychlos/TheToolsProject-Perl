34287ef3561695c4a6faf5970c3254588b8a561a
Pierre Wieser <pierre@pwi-consulting.fr>
2024-02-29 15:10:22 +0100

    ttp.pl vars names are camelCase

--- TheToolsProject/ttp/vars.do.pl
+++ TheToolsProject/ttp/vars.do.pl
@@ -4,9 +4,9 @@
 # @(-) --[no]verbose           run verbosely [${verbose}]
 # @(-) --[no]colored           color the output depending of the message level [${colored}]
 # @(-) --[no]dummy             dummy run (ignored here) [${dummy}]
-# @(-) --[no]logsdir           display the current Toops logs directory [${logsdir}]
-# @(-) --[no]logsroot          display the Toops logs Root (not daily) [${logsroot}]
-# @(-) --[no]siteroot          display the site-defined root path [${siteroot}]
+# @(-) --[no]siteRoot          display the site-defined root path [${siteRoot}]
+# @(-) --[no]logsRoot          display the Toops logs Root (not daily) [${logsRoot}]
+# @(-) --[no]logsDir           display the current Toops logs directory [${logsDir}]
 #
 # Copyright (@) 2023-2024 PWI Consulting
 
@@ -22,25 +22,17 @@ my $defaults = {
 	verbose => 'no',
 	colored => 'no',
 	dummy => 'no',
-	logsdir => 'no',
-	logsroot => 'no',
-	siteroot => 'no'
+	siteRoot => 'no',
+	logsRoot => 'no',
+	logsDir => 'no'
 };
 
-my $opt_logsroot = false;
-my $opt_logsdir = false;
-my $opt_siteroot = false;
+my $opt_siteRoot = false;
+my $opt_logsDir = false;
+my $opt_logsRoot = false;
 
 # -------------------------------------------------------------------------------------------------
-# list logsroot value - e.g. 'C:\INLINGUA\Logs'
-sub listLogsroot {
-	my $str = "logsRoot: $TTPVars->{config}{toops}{logsRoot}";
-	Mods::Toops::msgVerbose( "returning '$str'" );
-	print " $str".EOL;
-}
-
-# -------------------------------------------------------------------------------------------------
-# list logsdir value - e.g. 'C:\INLINGUA\Logs\240201\Toops'
+# list logsDir value - e.g. 'C:\INLINGUA\Logs\240201\Toops'
 sub listLogsdir {
 	my $str = "logsDir: $TTPVars->{config}{toops}{logsDir}";
 	Mods::Toops::msgVerbose( "returning '$str'" );
@@ -48,7 +40,15 @@ sub listLogsdir {
 }
 
 # -------------------------------------------------------------------------------------------------
-# list siteroot value - e.g. 'C:\INLINGUA'
+# list logsRoot value - e.g. 'C:\INLINGUA\Logs'
+sub listLogsroot {
+	my $str = "logsRoot: $TTPVars->{config}{toops}{logsRoot}";
+	Mods::Toops::msgVerbose( "returning '$str'" );
+	print " $str".EOL;
+}
+
+# -------------------------------------------------------------------------------------------------
+# list siteRoot value - e.g. 'C:\INLINGUA'
 sub listSiteroot {
 	my $str = "siteRoot: $TTPVars->{config}{site}{rootDir}";
 	Mods::Toops::msgVerbose( "returning '$str'" );
@@ -64,9 +64,9 @@ if( !GetOptions(
 	"verbose!"			=> \$TTPVars->{run}{verbose},
 	"colored!"			=> \$TTPVars->{run}{colored},
 	"dummy!"			=> \$TTPVars->{run}{dummy},
-	"logsdir!"			=> \$opt_logsdir,
-	"logsroot!"			=> \$opt_logsroot,
-	"siteroot!"			=> \$opt_siteroot )){
+	"siteRoot!"			=> \$opt_siteRoot,
+	"logsRoot!"			=> \$opt_logsRoot,
+	"logsDir!"			=> \$opt_logsDir )){
 
 		Mods::Toops::msgOut( "try '$TTPVars->{run}{command}{basename} $TTPVars->{run}{verb}{name} --help' to get full usage syntax" );
 		Mods::Toops::ttpExit( 1 );
@@ -80,14 +80,14 @@ if( Mods::Toops::wantsHelp()){
 Mods::Toops::msgVerbose( "found verbose='".( $TTPVars->{run}{verbose} ? 'true':'false' )."'" );
 Mods::Toops::msgVerbose( "found colored='".( $TTPVars->{run}{colored} ? 'true':'false' )."'" );
 Mods::Toops::msgVerbose( "found dummy='".( $TTPVars->{run}{dummy} ? 'true':'false' )."'" );
-Mods::Toops::msgVerbose( "found logsdir='".( $opt_logsdir ? 'true':'false' )."'" );
-Mods::Toops::msgVerbose( "found logsroot='".( $opt_logsroot ? 'true':'false' )."'" );
-Mods::Toops::msgVerbose( "found siteroot='".( $opt_siteroot ? 'true':'false' )."'" );
+Mods::Toops::msgVerbose( "found siteRoot='".( $opt_siteRoot ? 'true':'false' )."'" );
+Mods::Toops::msgVerbose( "found logsRoot='".( $opt_logsRoot ? 'true':'false' )."'" );
+Mods::Toops::msgVerbose( "found logsDir='".( $opt_logsDir ? 'true':'false' )."'" );
 
 if( !Mods::Toops::errs()){
-	listLogsdir() if $opt_logsdir;
-	listLogsroot() if $opt_logsroot;
-	listSiteroot() if $opt_siteroot;
+	listSiteroot() if $opt_siteRoot;
+	listLogsroot() if $opt_logsRoot;
+	listLogsdir() if $opt_logsDir;
 }
 
 Mods::Toops::ttpExit();
