2ff7d5facb2f3d1b297244b4b5f02b586559f215
Pierre Wieser <pierre@pwi-consulting.fr>
2024-03-08 14:34:03 +0100

    Standard workloads default to be enabled

--- SiteResources/ScheduledTasks/std-workloads.cmd
+++ SiteResources/ScheduledTasks/std-workloads.cmd
@@ -8,16 +8,16 @@ set JOB=daily.morning
 echo %JOB%
 @schtasks /Delete /TN Inlingua\%JOB% /F 1>NUL 2>NUL
 schtasks /Create /TN Inlingua\%JOB% /TR "%WORKLOAD% %JOB%" /SC DAILY /ST 05:00 /F %RUNNER%
-schtasks /Change /TN Inlingua\%JOB% /Disable
+schtasks /Change /TN Inlingua\%JOB% /Enable
 
 set JOB=daily.evening
 echo %JOB%
 schtasks /Delete /TN Inlingua\%JOB% /F 1>NUL 2>NUL
 schtasks /Create /TN Inlingua\%JOB% /TR "%WORKLOAD% %JOB%" /SC DAILY /ST 23:30 /F %RUNNER%
-schtasks /Change /TN Inlingua\%JOB% /Disable
+schtasks /Change /TN Inlingua\%JOB% /Enable
 
 set JOB=startup
 echo %JOB%
 schtasks /Delete /TN Inlingua\%JOB% /F 1>NUL 2>NUL
 schtasks /Create /TN Inlingua\%JOB% /TR "%WORKLOAD% %JOB%" /SC ONSTART /F %RUNNER%
-schtasks /Change /TN Inlingua\%JOB% /Disable
+schtasks /Change /TN Inlingua\%JOB% /Enable
