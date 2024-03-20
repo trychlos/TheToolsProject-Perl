edb77318b54fb322bf4a83f337f67235244d2267
Pierre Wieser <pierre@pwi-consulting.fr>
2024-03-20 11:19:48 +0100

    mswin.pl service: change the option names and better get the error message

--- TheToolsProject/mswin/service.do.pl
+++ TheToolsProject/mswin/service.do.pl
@@ -4,8 +4,8 @@
 # @(-) --[no]verbose           run verbosely [${verbose}]
 # @(-) --[no]colored           color the output depending of the message level [${colored}]
 # @(-) --[no]dummy             dummy run (ignored here) [${dummy}]
-# @(-) --service=<name>        apply to specified service [${service}]
-# @(-) --[no]query             query the service state [${query}]
+# @(-) --name=<name>           apply to the named service [${name}]
+# @(-) --[no]state             query the service state [${state}]
 # @(-) --[no]mqtt              publish the result as a MQTT telemetry [${mqtt}]
 # @(-) --[no]http              publish the result as a HTTP telemetry [${http}]
 #
@@ -25,70 +25,95 @@ my $defaults = {
 	verbose => 'no',
 	colored => 'no',
 	dummy => 'no',
-	service => '',
-	query => 'no',
+	name => '',
+	state => 'no',
 	mqtt => 'no',
 	http => 'no'
 };
 
-my $opt_service = $defaults->{service};
-my $opt_query = false;
+my $opt_name = $defaults->{name};
+my $opt_state = false;
 my $opt_mqtt = false;
 my $opt_http = false;
 
 # -------------------------------------------------------------------------------------------------
-# query the status of a service
-sub doServiceQuery {
-	msgOut( "querying the '$opt_service' service status..." );
-	my $command = "sc query $opt_service | findstr STATE";
+# query the status of a named service
+sub doServiceState {
+	msgOut( "querying the '$opt_name' service state..." );
+	my $command = "sc query $opt_name";
 	msgVerbose( $command );
 	my $stdout = `$command`;
 	my $rc = $?;
 	my $res = ( $rc == 0 );
 	msgVerbose( $stdout );
 	msgVerbose( "rc=$rc" );
+	# note that we do not should execute directly a pipe'd command as we want the return code of the 'sc' one
+	# find STATE the line if the command has been successful
+	# in case of an error we get the error message in the last non-blank line
+	my @lines = split( /[\r\n]/, $stdout );
+	my $count = scalar( @lines );
+	my $label = undef;
+	my $value = undef;
+	my $error = undef;
 	if( $res ){
-		my @words = split( /\s+/, $stdout );
-		my $label = $words[scalar( @words )-1];
-		my $value = "$words[scalar( @words )-2]";
-		msgOut( "  $value: $label" );
-		if( $opt_mqtt || $opt_http ){
-			my $dummy = $opt_dummy ? "-dummy" : "-nodummy";
-			my $verbose = $opt_verbose ? "-verbose" : "-noverbose";
-			if( $opt_mqtt ){
-				msgOut( "publishing to MQTT" );
-				$command = "telemetry.pl publish -metric state -value $label ".join( ' ', @ARGV )." -label role=$opt_service -mqtt -nohttp -nocolored $dummy $verbose";
+		my $state = undef;
+		foreach my $line ( @lines ){
+			if( $line =~ m/STATE/ ){
+				my @words = split( /\s+/, $stdout );
+				$label = $words[scalar( @words )-1];
+				$value = "$words[scalar( @words )-2]";
+				msgOut( "  $value: $label" );
+			}
+		}
+	} else {
+		for( my $i=$count ; $i ; --$i ){
+			if( $lines[$i-1] && length $lines[$i-1] ){
+				$error = $lines[$i-1];
+				last;
+			}
+		}
+		if( !defined $error ){
+			$error = "Undefined error";
+		}
+		msgErr( $error );
+	}
+	# publish the result in all cases, and notably even if there was an error
+	if( $opt_mqtt || $opt_http ){
+		my $dummy = $opt_dummy ? "-dummy" : "-nodummy";
+		my $verbose = $opt_verbose ? "-verbose" : "-noverbose";
+		my $metric_value = undef;
+		if( $opt_mqtt ){
+			msgOut( "publishing to MQTT" );
+			$metric_value = $res ? $label : $error;
+			$command = "telemetry.pl publish -metric state -value $metric_value ".join( ' ', @ARGV )." -label role=$opt_name -mqtt -nohttp -nocolored $dummy $verbose";
+			msgVerbose( $command );
+			$stdout = `$command`;
+			$rc = $?;
+			msgVerbose( $stdout );
+			msgVerbose( "rc=$rc" );
+		}
+		if( $opt_http ){
+			msgOut( "publishing to HTTP" );
+			# Source: https://learn.microsoft.com/en-us/windows/win32/services/service-status-transitions
+			my $states = {
+				'1' => 'stopped',
+				'2' => 'start_pending',
+				'3' => 'stop_pending',
+				'4' => 'running',
+				'5' => 'continue_pending',
+				'6' => 'pause_pending',
+				'7' => 'paused'
+			};
+			foreach my $key ( keys( %{$states} )){
+				$metric_value = ( defined $value && $key eq $value ) ? "1" : "0";
+				$command = "telemetry.pl publish -metric ttp_service_daemon -value $metric_value ".join( ' ', @ARGV )." -label role=$opt_name -label state=$states->{$key} -nomqtt -http -nocolored $dummy $verbose";
 				msgVerbose( $command );
 				$stdout = `$command`;
 				$rc = $?;
 				msgVerbose( $stdout );
 				msgVerbose( "rc=$rc" );
 			}
-			if( $opt_http ){
-				msgOut( "publishing to HTTP" );
-				# Source: https://learn.microsoft.com/en-us/windows/win32/services/service-status-transitions
-				my $states = {
-					'1' => 'stopped',
-					'2' => 'start_pending',
-					'3' => 'stop_pending',
-					'4' => 'running',
-					'5' => 'continue_pending',
-					'6' => 'pause_pending',
-					'7' => 'paused'
-				};
-				foreach my $key ( keys( %{$states} )){
-					my $metric_value = ( $key eq $value ) ? "1" : "0";
-					$command = "telemetry.pl publish -metric ttp_service_daemon -value $metric_value ".join( ' ', @ARGV )." -label role=$opt_service -label state=$states->{$key} -nomqtt -http -nocolored $dummy $verbose";
-					msgVerbose( $command );
-					$stdout = `$command`;
-					$rc = $?;
-					msgVerbose( $stdout );
-					msgVerbose( "rc=$rc" );
-				}
-			}
 		}
-	} else {
-		msgWarn( "most probably, the service doesn't exist" );;
 	}
 	if( $res ){
 		msgOut( "success" );
@@ -106,8 +131,8 @@ if( !GetOptions(
 	"verbose!"			=> \$TTPVars->{run}{verbose},
 	"colored!"			=> \$TTPVars->{run}{colored},
 	"dummy!"			=> \$TTPVars->{run}{dummy},
-	"service=s"			=> \$opt_service,
-	"query!"			=> \$opt_query,
+	"name=s"			=> \$opt_name,
+	"state!"			=> \$opt_state,
 	"mqtt!"				=> \$opt_mqtt,
 	"http!"				=> \$opt_http )){
 
@@ -123,16 +148,16 @@ if( Mods::Toops::wantsHelp()){
 msgVerbose( "found verbose='".( $TTPVars->{run}{verbose} ? 'true':'false' )."'" );
 msgVerbose( "found colored='".( $TTPVars->{run}{colored} ? 'true':'false' )."'" );
 msgVerbose( "found dummy='".( $TTPVars->{run}{dummy} ? 'true':'false' )."'" );
-msgVerbose( "found service='$opt_service'" );
-msgVerbose( "found query='".( $opt_query ? 'true':'false' )."'" );
+msgVerbose( "found name='$opt_name'" );
+msgVerbose( "found state='".( $opt_state ? 'true':'false' )."'" );
 msgVerbose( "found mqtt='".( $opt_mqtt ? 'true':'false' )."'" );
 msgVerbose( "found http='".( $opt_http ? 'true':'false' )."'" );
 
 # a service name is mandatory when querying its status
-msgErr( "a service name is mandatory when querying for a status" ) if $opt_query && !$opt_service;
+msgErr( "'--name' service name is mandatory when querying for a status" ) if $opt_state && !$opt_name;
 
 if( !ttpErrs()){
-	doServiceQuery() if $opt_service && $opt_query;
+	doServiceState() if $opt_name && $opt_state;
 }
 
 ttpExit();
