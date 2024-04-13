2f3fbab9e01686ad144ea45a2cab991eee767475
Pierre Wieser <pierre@pwi-consulting.fr>
2024-04-13 16:18:59 +0200

    services.pl live: review the published http metrics

--- TheToolsProject/services/live.do.pl
+++ TheToolsProject/services/live.do.pl
@@ -6,7 +6,7 @@
 # @(-) --[no]dummy             dummy run (ignored here) [${dummy}]
 # @(-) --service=<name>        the named service [${service}]
 # @(-) --environment=<type>    the searched for environment [${environment}]
-# @(-) --[no]next              also search for next machine [${next}]
+# @(-) --[no]next              also search for next machine(s) [${next}]
 # @(-) --[no]mqtt              publish MQTT telemetry [${mqtt}]
 # @(-) --[no]http              publish HTTP telemetry [${http}]
 #
@@ -62,8 +62,10 @@ sub getLive {
 		my @words = split( /\s+/, $it );
 		push( @hosts, $words[scalar( @words )-1] );
 	}
+	# @hosts holds the list of hosts which say they host the service for the environment
+	# we may find at most one live, and maybe several backups
 	my $live = undef;
-	my $next = undef;
+	my @nexts = ();
 	foreach my $host ( @hosts ){
 		msgVerbose( "examining '$host'" );
 		my $hostConfig = Mods::Toops::getHostConfig( $host );
@@ -83,39 +85,46 @@ sub getLive {
 						my @words = split( /\s+/, $output[0] );
 						$live = $words[scalar( @words )-1];
 						print "  live: $live".EOL;
-						if( $opt_next ){
-							my @nexts = grep( !/$live/, @hosts );
-							$next = $nexts[0];
-							print "  next: $next".EOL;
-						}
 					}
+				} else {
+					msgVerbose( "the service seems unable to identify its own live machine, saying there is none (maybe is it dead ?)" );
 				}
 				last;
 			}
 		}
 	}
-	my $labels = "";
-	$labels = "-label service=$opt_service -label environment=$opt_environment";
+	if( $opt_next ){
+		@nexts = $live ? grep( !/$live/, @hosts ) : @hosts;
+		foreach my $next ( @nexts ){
+			print "  next: $next".EOL;
+		}
+	}
+	my $labels = "-label service=$opt_service -label environment=$opt_environment";
+	my $next = join( ',', @nexts );
 	if( $opt_mqtt ){
 		# topic is HOST/telemetry/service/SERVICE/environment/ENVIRONMENT/machine/live=live
 		# topic is HOST/telemetry/service/SERVICE/environment/ENVIRONMENT/machine/next=next
-		if( $live ){
-			$command = "telemetry.pl publish -metric live $labels -value=$live -mqtt -mqttPrefix machine/ -nohttp";
-			`$command`;
-		}
-		if( $opt_next && $next ){
+		my $mqtt_live = $live || 'none';
+		$command = "telemetry.pl publish -metric live $labels -value=$mqtt_live -mqtt -mqttPrefix machine/ -nohttp";
+		`$command`;
+		if( $opt_next && scalar @nexts ){
 			$command = "telemetry.pl publish -metric backup $labels -value=$next -mqtt -mqttPrefix machine/ -nohttp";
 			`$command`;
 		}
 	}
 	if( $opt_http ){
-		# set the value "1" when we are running of the live host
+		# set the value "1" on the live metric when we have found one (i.e. is not undef)
+		# we publish here one metric per host for the service and the environment with
+		# - either a 'live' label if the host if the live host
+		# - or a 'backup' label if the host is a backup host
 		my $running = ttpHost();
+		msgVerbose( "runningHost is '$running'" );
 		foreach my $host ( @hosts ){
-			my $value = ( $live && $live eq $running ) ? "1" : "0";
+			my $value = ( $live && $live eq $host ) ? "1" : "0";
 			my $httpLabels = $labels;
-			$httpLabels .= " -label live=$live" if $live;
-			$httpLabels .= " -label backup=$next" if $next;
+			my $http_live = $live || 'none';
+			$httpLabels .= " -label live=$host" if $value eq "1";
+			$httpLabels .= " -label backup=$host" if grep( /$host/, @nexts );
 			$command = "telemetry.pl publish -metric ttp_service_machine $httpLabels -value=$value -nomqtt -http";
 			`$command`;
 		}
