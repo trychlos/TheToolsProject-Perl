3b53520d0b3961f7b549c99df98fb6996f0d1699
Pierre Wieser <pierre@pwi-consulting.fr>
2024-05-06 19:09:39 +0200

    ttp.pl writejson may take advantage of a templating system filenaming (todo #30)

--- TheToolsProject/ttp/writejson.do.pl
+++ TheToolsProject/ttp/writejson.do.pl
@@ -5,6 +5,9 @@
 # @(-) --[no]dummy             dummy run (ignored here) [${dummy}]
 # @(-) --[no]verbose           run verbosely [${verbose}]
 # @(-) --file=<filename>       the filename where to write the data [${file}]
+# @(-) --dir=<dir>             the directory where to create the file [${dir}]
+# @(-) --template=<template>   the filename template [${template}]
+# @(-) --suffix=<suffix>       the filename suffix [${suffix}]
 # @(-) --data=<data>           the data to be written as a JSON string [${data}]
 # @(-) --[no]append            whether to append to the file [${append}]
 #
@@ -26,6 +29,7 @@
 # along with The Tools Project; see the file COPYING. If not,
 # see <http://www.gnu.org/licenses/>.
 
+use File::Temp;
 use JSON;
 
 my $defaults = {
@@ -34,11 +38,17 @@ my $defaults = {
 	dummy => 'no',
 	verbose => 'no',
 	file => '',
+	dir => '',
+	template => '',
+	suffix => '.json',
 	data => '{}',
 	append => 'no'
 };
 
 my $opt_file = $defaults->{file};
+my $opt_dir = $defaults->{dir};
+my $opt_template = $defaults->{template};
+my $opt_suffix = $defaults->{suffix};
 my $opt_data = $defaults->{data};
 my $opt_append = false;
 
@@ -51,6 +61,17 @@ sub doWriteJson {
 	my $res = false;
 	my $json = JSON->new;
 	my $data = $json->decode( $opt_data );
+	# if no filename is provided, compute one with maybe a dir, maybe a template, maybe a suffix
+	if( !$opt_file ){
+		my %parms = ();
+		$parms{DIR} = $opt_dir if $opt_dir;
+		$parms{TEMPLATE} = $opt_template if $opt_template;
+		$parms{SUFFIX} = $opt_suffix if $opt_suffix;
+		$parms{UNLINK} = false;
+		my $tmp = File::Temp->new( %parms );
+		$opt_file = $tmp->filename();
+		msgVerbose( "setting opt_file tp '$opt_file'" );
+	}
 	if( $opt_append ){
 		$res = TTP::jsonAppend( $data, $opt_file );
 	} else {
@@ -73,6 +94,9 @@ if( !GetOptions(
 	"dummy!"			=> \$ttp->{run}{dummy},
 	"verbose!"			=> \$ttp->{run}{verbose},
 	"file=s"			=> \$opt_file,
+	"dir=s"				=> \$opt_dir,
+	"template=s"		=> \$opt_template,
+	"suffix=s"			=> \$opt_suffix,
 	"data=s"			=> \$opt_data,
 	"append!"			=> \$opt_append )){
 
@@ -89,12 +113,16 @@ msgVerbose( "found colored='".( $running->colored() ? 'true':'false' )."'" );
 msgVerbose( "found dummy='".( $running->dummy() ? 'true':'false' )."'" );
 msgVerbose( "found verbose='".( $running->verbose() ? 'true':'false' )."'" );
 msgVerbose( "found file='$opt_file'" );
+msgVerbose( "found dir='$opt_dir'" );
+msgVerbose( "found template='$opt_template'" );
+msgVerbose( "found suffix='$opt_suffix'" );
 msgVerbose( "found data='$opt_data'" );
 msgVerbose( "found append='".( defined $opt_append ? ( $opt_append ? 'true':'false' ) : '(undef)' )."'" );
 
-# all data are mandatory, and we must provide some content, either text or html
-msgErr( "file is mandatory, not specified" ) if !$opt_file;
+# data is mandatory
 msgErr( "data is mandatory, not specified" ) if !$opt_data;
+# a filename is mandatory if we want to append to it
+msgErr( "filename is mandatory to be able to append to it, not specified" ) if !$opt_file && $opt_append;
 
 if( !TTP::errs()){
 	doWriteJson();
